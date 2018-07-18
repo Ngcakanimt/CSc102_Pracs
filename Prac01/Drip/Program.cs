f4d4aa0614658288d6e2e7f316e7b8ebfb5c46fb
Ngcakanimt <mthangakani2@gmail.com>
2018-07-18 16:10:26 +0200

    This is a back up

---
+++ Prac01/Drip/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How often do you here a drip of water?");
+            string input_str = Console.ReadLine();
+            double n = Convert.ToDouble(input_str);
+            double hour = 3600 / n * 0.00025;
+            double days = 86400 / n * 0.00025;
+            double week = 604800 / n * 0.00025;
+            Console.WriteLine("This wastes "+hour+" litres of water every hour");
+            Console.WriteLine("This wastes "+days+" litres of water every day");
+            Console.WriteLine("THis wastes "+week+" litres of water every week");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Prac01/Wholeglasses/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wholeglasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many litres do you use?");
+            string user = Console.ReadLine();
+            double conv = Convert.ToDouble(user);
+            double n = conv * 1000 / 250;
+            Console.WriteLine("You use " + n + " glasses of water");
+            Console.ReadLine();
+        }
+    }
+}
