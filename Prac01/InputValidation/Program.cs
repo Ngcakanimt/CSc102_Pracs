4ef58343aa2ac5ca1a4ac213c62d562a8275fcac
Ngcakanimt <mthangakani2@gmail.com>
2018-07-21 21:05:17 +0200

    This is a backup

---
+++ Prac01/InputValidation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many litres do you use?");
+            string user = Console.ReadLine();
+            int.TryParse(user, out int result);
+            double conv = Convert.ToDouble(user);
+            double n = conv * 1000 / 250;
+            Console.WriteLine("You use " + n + " glasses of water");
+            Console.ReadLine();
+        }
+    }
+}
