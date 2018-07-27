6e791dac6da4cf31a64c6827253f8ea0bcbe767c
Ngcakanimt <mthangakani2@gmail.com>
2018-07-27 14:56:05 +0200

    Back up

---
+++ Prac02_Classwork/ChangeCalculator/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("----Please enter an amount: ");
+            string Amount = Console.ReadLine();
+            double amount;
+            bool amnt = double.TryParse(Amount, out amount);
+
+            if (amnt && amount >= 0)
+            {
+                amount = Math.Round(amount, 2);
+
+                while (amount > 0)
+                {
+
+                    if (amount >= 200)
+                    {
+
+                        double ans2 = amount / 200;
+                        ans2 = Math.Floor(ans2);
+                        amount = amount - (ans2 * 200);
+                        Console.WriteLine("Return {0} x R200 note<s>", ans2);
+                        amount = Math.Round(amount, 2);
+
+                    }
+                    else if (amount >= 100)
+                    {
+
+                        double ans2 = amount / 100;
+                        ans2 = Math.Floor(ans2);
+                        amount = amount - (ans2 * 100);
+                        Console.WriteLine("Return {0} x R100 note<s>", ans2);
+                        amount = Math.Round(amount, 2);
+                    }
+                    else if (amount >= 50)
+                    {
+
+                        double ans2 = amount / 50;
+                        ans2 = Math.Floor(ans2);
+                        amount = amount - (ans2 * 50);
+                        Console.WriteLine("Return {0} x R50 note<s>", ans2);
+                        amount = Math.Round(amount, 2);
+                    }
+                    else if (amount >= 20)
+                    {
+
+                        double ans2 = (amount / 20);
+                        ans2 = Math.Floor(ans2);
+                        amount = amount - (ans2 * 20);
+                        Console.WriteLine("Return {0} x R20 note<s>", ans2);
+                        amount = Math.Round(amount, 2);
+                    }
+                    else if (amount >= 10)
+                    {
+
+                        double ans2 = amount / 10;
+                        ans2 = Math.Floor(ans2);
+                        amount = amount - (ans2 * 10);
+                        Console.WriteLine("Return {0} x R10 note<s>", ans2);
+                        amount = Math.Round(amount, 2);
+                    }
+                    else if (amount >= 5)
+                    {
+
+
+                        double ans2 = amount / 5;
+                        ans2 = Math.Floor(ans2);
+                        amount = amount - (ans2 * 5);
+                        Console.WriteLine("Return {0} x 5 note<s>", ans2);
+                        amount = Math.Round(amount, 2);
+                    }
+                    else if (amount >= 2)
+                    {
+
+                        double ans2 = amount / 2;
+                        ans2 = Math.Floor(ans2);
+                        amount = amount - (ans2 * 2);
+                        Console.WriteLine("Return {0} x R2 note<s>", ans2);
+                        amount = Math.Round(amount, 2);
+
+                    }
+                    else if (amount >= 1)
+                    {
+
+                        double ans2 = amount / 1;
+                        ans2 = Math.Floor(ans2);
+                        amount = amount - (ans2 * 1);
+                        Console.WriteLine("Return {0} x R1 note<s>", ans2);
+                        amount = Math.Round(amount, 2);
+                    }
+                    else if (amount >= 0.50)
+                    {
+
+                        double ans2 = amount / 0.50;
+                        ans2 = Math.Floor(ans2);
+                        amount = amount - (ans2 * 0.50);
+                        Console.WriteLine("Return {0} x 50c note<s>", ans2);
+                        amount = Math.Round(amount, 2);
+                    }
+                    else if (amount >= 0.20)
+                    {
+
+                        double ans2 = amount / 0.20;
+                        ans2 = Math.Floor(ans2);
+                        amount = amount - (ans2 * 0.20);
+                        Console.WriteLine("Return {0} x 20c note<s>", ans2);
+                        amount = Math.Round(amount, 2);
+                    }
+                    else if (amount >= 0.10)
+                    {
+
+                        double ans2 = amount / 0.10;
+                        ans2 = Math.Floor(ans2);
+                        amount = amount - (ans2 * 0.10);
+                        Console.WriteLine("Return {0} x 10c note<s>", ans2);
+                        amount = Math.Round(amount, 2);
+                    }
+                    else if (amount >= 0.05)
+                    {
+
+
+                        double ans2 = amount / 0.05;
+                        amount = amount - 0.05;
+                        ans2 = Math.Floor(ans2);
+                        Console.WriteLine("Return {0} x 5c note<s>", ans2);
+                        amount = Math.Round(amount, 2);
+
+                    }
+                    else if (amount >= 0.02)
+                    {
+
+                        double ans2 = amount / 0.02;
+                        ans2 = Math.Floor(ans2);
+                        amount = amount - 0.02;
+                        Console.WriteLine("Return {0} x 2c note<s>", ans2);
+                        amount = Math.Round(amount, 2);
+
+                    }
+                    else if (amount >= 0.01)
+                    {
+                        double ans2 = amount / 0.01;
+                        ans2 = Math.Floor(ans2);
+                        amount = amount - 0.01;
+                        Console.WriteLine("Return {0} x 1c note<s>", ans2);
+                        amount = Math.Round(amount, 2);
+                    }
+                }
+                Console.WriteLine("please press ENTER to continue. . . . ");
+                Console.ReadKey();
+                Main(args);
+
+            }
+            else
+            {
+                Console.WriteLine("invalid number");
+                Console.WriteLine("please press ENTER to continue. . . . ");
+                Console.ReadKey();
+                Main(args);
+            }
+
+
+        }
+    }
+}
---
+++ Prac02_Classwork/TaxCalculator/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxesCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your monthly salary: ");
+            string input_salary = Console.ReadLine();
+            Console.WriteLine("Please enter your age: ");
+            string input_age = Console.ReadLine();
+            int salary = Convert.ToInt32(input_salary);
+            int age = Convert.ToInt32(input_age);
+
+            if (salary > 0 && age > 0)
+                if (salary >= 0 && salary <= 4999)
+                {
+                    if (age < 65)
+                        Console.WriteLine("Your monthly tax is 0");
+                    if (age >= 65 && age < 75)
+                        Console.WriteLine("Your monthly tax is 0");
+                    if (age > 75)
+                        Console.WriteLine("Your monthly tax is 0");
+                    Console.ReadLine();
+                    Console.WriteLine("Press ENTER to continue...");
+                }
+
+                else if (salary >= 5000 && salary <= 6499)
+                {
+                    if (age < 65)
+                        Console.WriteLine("Your monthly tax is 13");
+                    if (age >= 65 && age < 75)
+                        Console.WriteLine("Your monthly tax is 0");
+                    if (age > 75)
+                        Console.WriteLine("Your monthly tax is 0");
+                    Console.ReadLine();
+                    Console.WriteLine("Press ENTER to continue...");
+
+                }
+
+                else if (salary >= 6500 && salary <= 7799)
+                {
+                    if (age < 65)
+                        Console.WriteLine("Your monthly tax is 250");
+                    if (age >= 65 && age < 75)
+                        Console.WriteLine("Your monthly tax is 0");
+                    if (age > 75)
+                        Console.WriteLine("Your monthly tax is 0");
+                    Console.ReadLine();
+                    Console.WriteLine("Press ENTER to continue...");
+                }
+
+                else if (salary >= 7800 && salary <= 9999)
+                {
+                    if (age < 65)
+                        Console.WriteLine("Your monthly tax is 620");
+                    if (age >= 65 && age < 75)
+                        Console.WriteLine("Your monthly tax is 0");
+                    if (age > 75)
+                        Console.WriteLine("Your monthly tax is 0");
+                    Console.ReadLine();
+                    Console.WriteLine("Press ENTER to continue...");
+                }
+                else if (salary >= 10000 && salary <= 13499)
+                {
+                    if (age < 65)
+                        Console.WriteLine("Your monthly tax is 1200");
+                    if (age >= 65 && age < 75)
+                        Console.WriteLine("Your monthly tax is 600");
+                    if (age > 75)
+                        Console.WriteLine("Your monthly tax is 400");
+                    Console.ReadLine();
+                    Console.WriteLine("Press ENTER to continue...");
+                }
+                else if (salary >= 13500 && salary <= 17899)
+                {
+                    if (age < 65)
+                        Console.WriteLine("Your monthly tax is 2100");
+                    if (age >= 65 && age < 75)
+                        Console.WriteLine("Your monthly tax is 1500");
+                    if (age > 75)
+                        Console.WriteLine("Your monthly tax is 1200");
+                    Console.ReadLine();
+                    Console.WriteLine("Press ENTER to continue...");
+                }
+                else if (salary >= 17900 && salary <= 19999)
+                {
+                    if (age < 65)
+                        Console.WriteLine("Your monthly tax is 2700");
+                    if (age >= 65 && age < 75)
+                        Console.WriteLine("Your monthly tax is 2000");
+                    if (age > 75)
+                        Console.WriteLine("Your monthly tax is 1800");
+                    Console.ReadLine();
+                    Console.WriteLine("Press ENTER to continue...");
+                }
+                else if (salary >= 20000 && salary <= 39999)
+                {
+                    if (age < 65)
+                        Console.WriteLine("Your monthly tax is 8000");
+                    if (age >= 65 && age < 75)
+                        Console.WriteLine("Your monthly tax is 7500");
+                    if (age > 75)
+                        Console.WriteLine("Your monthly tax is 7300");
+                    Console.ReadLine();
+                    Console.WriteLine("Press ENTER to continue...");
+                }
+                else if (salary >= 40000 && salary <= 59999)
+                {
+                    if (age < 65)
+                        Console.WriteLine("Your monthly tax is 16000");
+                    if (age >= 65 && age < 75)
+                        Console.WriteLine("Your monthly tax is 15600");
+                    if (age > 75)
+                        Console.WriteLine("Your monthly tax is 15400");
+                    Console.ReadLine();
+                    Console.WriteLine("Press ENTER to continue...");
+                }
+                else if (salary >= 60000)
+                {
+                    if (age < 65)
+                        Console.WriteLine("Your monthly tax is 20000");
+                    if (age >= 65 && age < 75)
+                        Console.WriteLine("Your monthly tax is 17050");
+                    if (age > 75)
+                        Console.WriteLine("Your monthly tax is 17000");
+                    Console.ReadLine();
+                    Console.WriteLine("Press ENTER to continue...");
+
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
